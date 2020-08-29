c8f24f97ba417fb6088fc3aa03748c9b68d0fc43
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2020-08-29 22:34:50 +0530

    Updates

---
+++ precache-manifest.16fd7c6da0a886f78cda7bce22df1d04.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09e2269123ed08251a81af018616a20",
+    "url": "/avinash-portfolio/index.html"
+  },
+  {
+    "revision": "a9e8976d5270f358fc6f",
+    "url": "/avinash-portfolio/static/css/main.dca2b689.chunk.css"
+  },
+  {
+    "revision": "f31bb4e76a146fc206ef",
+    "url": "/avinash-portfolio/static/js/2.87faa6a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avinash-portfolio/static/js/2.87faa6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e8976d5270f358fc6f",
+    "url": "/avinash-portfolio/static/js/main.a222fc07.chunk.js"
+  },
+  {
+    "revision": "2b4a9e468390c77c27f7",
+    "url": "/avinash-portfolio/static/js/runtime-main.39c123b8.js"
+  },
+  {
+    "revision": "95dca2bc6dae48632522f91a0b953921",
+    "url": "/avinash-portfolio/static/media/0.95dca2bc.png"
+  },
+  {
+    "revision": "9d59a3638fdb8d1a12f567e58d80212e",
+    "url": "/avinash-portfolio/static/media/1.9d59a363.png"
+  },
+  {
+    "revision": "d0b28c86040ddf1351b93efb2cf56c2e",
+    "url": "/avinash-portfolio/static/media/2.d0b28c86.png"
+  },
+  {
+    "revision": "95dca2bc6dae48632522f91a0b953921",
+    "url": "/avinash-portfolio/static/media/3.95dca2bc.png"
+  }
+]);
