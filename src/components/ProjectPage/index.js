d137537e47222e93fd5b958c6f6d10b1032ce809
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2020-09-04 01:24:02 +0530

    Write project page footer and image train

--- src/components/ProjectPage/index.js
+++ src/components/ProjectPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ProjectHeader from './ProjectPageHeader/'
+import ProjectFooter from './ProjectPageFooter/'
 import CloseButton from './../CloseButton/'
 
 export default function ProjectPage(props) {
@@ -9,6 +10,20 @@ export default function ProjectPage(props) {
 			<CloseButton />
 			<ProjectHeader title="Mangoes & Marbles" category="Animated Short Film" 
 				timestamp="June, 2020" description="Project Description. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in ad deleniti minima non dolorem, porro libero veniam tempore natus fugiat error veritatis rerum. Dolore voluptas nisi repellendus magnam esse?" />
+
+
+
+
+			<div className="image-train" style={{gridColumn: 1/-1, flexDirection:"column"}}>
+				<img class=" image-train-images " src="https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/30/2560x1280/landscape-1500925839-golden-retriever-puppy.jpg?resize=480:*"/>
+				<img class=" image-train-images " src="https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/30/2560x1280/landscape-1500925839-golden-retriever-puppy.jpg?resize=480:*"/>
+				<img class=" image-train-images " src="https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/30/2560x1280/landscape-1500925839-golden-retriever-puppy.jpg?"/>
+			</div>
+
+
+
+
+			<ProjectFooter />
 		</div>
 	);
 }
